c99eedfcc97c1ad4f6730022fd36c68a2c7c8d9a
João Paulo <devjoaomoreira@gmail.com>
2022-06-17 15:10:35 -0300

    --Change: Function to Class and This return fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import InitScrollAnimation from "./Modules/scroll-animation.js";
 import TabNav from "./Modules/Tabnavigation.js";
-import InitModal from "./Modules/modal.js";
+import Modal from "./Modules/modal.js";
 import ToolTip from "./Modules/tooltip.js";
 import DropdownMenu from "./Modules/dropdown-menu.js";
 import MenuMobile from "./Modules/menu-mobile.js";
@@ -9,14 +9,17 @@ import initFuncionamento from "./Modules/funcionamento.js";
 import FetchBit from "./Modules/InitFetchBitcoin.js";
 import Accordion from "./Modules/Accordion.js";
 
+
 const accordion = new Accordion('#faq dt');
 accordion.init();
 
 const tabNav = new TabNav('[data-tab="menu"] li','[data-tab="content"] section');
 tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]')
+modal.init();
+
 InitScrollAnimation();
-InitModal();
 ToolTip();
 DropdownMenu();
 MenuMobile();
