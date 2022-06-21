4074171f67e5be3de1846ba705fd81d18e60b63b
João Paulo <devjoaomoreira@gmail.com>
2022-06-21 19:19:15 -0300

    --Change: melhorias no código do Tooltip

--- js/Modules/tooltip.js
+++ js/Modules/tooltip.js
@@ -2,26 +2,32 @@ export default class Tooltip {
   constructor(tooltips){
     this.tooltips = document.querySelectorAll(tooltips);
     this.MouseOver = this.MouseOver.bind(this);
+    this.MouseMove = this.MouseMove.bind(this);
+    this.MouseLeave = this.MouseLeave.bind(this);
   }
 
   //Inicia os eventos de mover o mouse e de retirar o cursor
-  MouseOver(e,element){
-    const Box = this.CreateBox(element);
-    element.addEventListener('mousemove',(evento)=> this.MouseMove(evento,Box));
-    element.addEventListener('mouseleave',()=> this.MouseLeave(Box,element)); 
+  MouseOver( {currentTarget} ){
+    this.CreateBox(currentTarget);
+    currentTarget.addEventListener('mousemove',this.MouseMove);
+    currentTarget.addEventListener('mouseleave',this.MouseLeave); 
   }
 
   //É verificado toda vez que movermos o mouse
   //a posição do cursor e é jogado para o top e left da Box
-  MouseMove(event,Box){
-        Box.style.top = event.pageY + 20 + 'px';
-        Box.style.left = event.pageX + 20 + 'px';
+  MouseMove(event){
+        this.box.style.top = event.pageY + 20 + 'px';
+  if(event.pageX + 200 > window.innerWidth){
+    this.box.style.left = event.pageX - 150 + 'px';
+      }else{
+          this.box.style.left = event.pageX + 20 + 'px';
+        }
   }
 
-  MouseLeave(Box,Element){
-    Box.remove();
-    Element.removeEventListener('mousemove',this.MouseMove)
-    Element.removeEventListener('mouseleave',this.MouseLeave)
+  MouseLeave(event){
+    this.box.remove();
+    event.currentTarget.removeEventListener('mousemove',this.MouseMove)
+    event.currentTarget.removeEventListener('mouseleave',this.MouseLeave)
 }
 
 //Cria uma nova Box com
@@ -31,12 +37,12 @@ export default class Tooltip {
     box.classList.add('tooltip');
     box.innerHTML = element.getAttribute('aria-label');
     document.body.appendChild(box);
-    return box;
+    this.box = box;
   }
 
   init(){
     if(this.tooltips.length)
-    this.tooltips.forEach((tool) => tool.addEventListener("mouseover",(e)=> this.MouseOver(e,tool)));
+    this.tooltips.forEach((tool) => tool.addEventListener("mouseover",this.MouseOver));
     return this;
   }
 }
